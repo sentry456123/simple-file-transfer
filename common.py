57dea69a6d2bbf52e7b3a6d445f0506c978c21c8
sentry456123 <92769408+sentry456123@users.noreply.github.com>
2022-12-24 15:51:36 +0900

    Update common.py

--- common.py
+++ common.py
@@ -4,7 +4,6 @@ IP = socket.gethostbyname(socket.gethostname())
 PORT = 9999
 ADDR = (IP, PORT)
 MAX_SIZE = 1024
-FMT = "utf-8"
 
 def clamp(n, smallest, largest):
     return max(smallest, min(n, largest))
